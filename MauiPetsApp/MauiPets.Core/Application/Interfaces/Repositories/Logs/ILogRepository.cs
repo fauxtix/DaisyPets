d7984a7ad5b1d3289048e3621e941d0c78f9cc2f
fauxtix <fauxtix.luix@hotmail.com>
2024-09-25 13:18:40 +0100

    Logs

---
+++ MauiPetsApp/MauiPets.Core/Application/Interfaces/Repositories/Logs/ILogRepository.cs
@@ -0,0 +1,11 @@
+﻿using MauiPets.Core.Application.ViewModels.Logs;
+
+namespace MauiPets.Core.Application.Interfaces.Repositories.Logs
+{
+    public interface ILogRepository
+    {
+        Task<IEnumerable<LogEntry>> GetLogsAsync(int page, int pageSize);
+        Task<int> GetLogCountAsync();
+        Task<IEnumerable<LogEntry>> GetFilteredLogsAsync(string searchText);
+    }
+}
