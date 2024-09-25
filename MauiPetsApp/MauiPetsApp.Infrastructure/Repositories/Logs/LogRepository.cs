66d999955267aa9ea892c1ab7c8398f0d3328c10
fauxtix <fauxtix.luix@hotmail.com>
2024-09-25 21:03:39 +0100

    Log repository

---
+++ MauiPetsApp/MauiPetsApp.Infrastructure/Repositories/Logs/LogRepository.cs
@@ -0,0 +1,68 @@
+﻿using Dapper;
+using MauiPets.Core.Application.Interfaces.Repositories.Logs;
+using MauiPets.Core.Application.ViewModels.Logs;
+using MauiPetsApp.Core.Application.Interfaces.DapperContext;
+using Serilog;
+
+namespace MauiPetsApp.Infrastructure.Repositories.Logs
+{
+    public class LogRepository : ILogRepository
+    {
+        private readonly IDapperContext _context;
+
+        public LogRepository(IDapperContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<LogEntry>> GetLogsAsync(int page, int pageSize)
+        {
+            try
+            {
+                using var connection = _context.CreateConnection();
+                var offset = (page - 1) * pageSize;
+                var sql = "SELECT * FROM PetsLogs ORDER BY date(TimeStamp) DESC LIMIT @PageSize OFFSET @Offset";
+                var output = await connection.QueryAsync<LogEntry>(sql, new { PageSize = pageSize, Offset = offset });
+                return output;
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"An error occurred: {ex.Message}");
+                return Enumerable.Empty<LogEntry>();
+            }
+        }
+
+        public async Task<int> GetLogCountAsync()
+        {
+            using var connection = _context.CreateConnection();
+            var sql = "SELECT COUNT(*) FROM PetsLogs";
+
+            return await connection.QuerySingleOrDefaultAsync<int>(sql);
+        }
+
+        public async Task<IEnumerable<LogEntry>> GetFilteredLogsAsync(string searchText)
+        {
+            using var connection = _context.CreateConnection();
+            var sql = "SELECT * FROM PetsLogs WHERE Message LIKE @SearchText ORDER BY date(TimeStamp) DESC";
+
+            return await connection.QueryAsync<LogEntry>(sql, new { SearchText = $"%{searchText}%" });
+        }
+
+        public async Task DeleteLogsAsync()
+        {
+            try
+            {
+                using var connection = _context.CreateConnection();
+                var sql = "DELETE FROM PetsLogs";
+                await connection.ExecuteAsync(sql);
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Erro ao apagar Logs {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
