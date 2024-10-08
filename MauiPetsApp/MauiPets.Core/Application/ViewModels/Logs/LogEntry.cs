14f7dd950e6a9fec9877e22b6abdaa0ff5e0acd5
fauxtix <fauxtix.luix@hotmail.com>
2024-10-08 18:05:54 +0100

    Commit

---
+++ MauiPetsApp/MauiPets.Core/Application/ViewModels/Logs/LogEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiPets.Core.Application.ViewModels.Logs
+{
+    public class LogEntry
+    {
+        public int Id { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string Level { get; set; } = string.Empty;
+        public DateTime TimeStamp { get; set; }
+        public string Exception { get; set; } = string.Empty;
+        public string Properties { get; set; } = string.Empty;
+    }
+}
